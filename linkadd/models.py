6b1000ec5bb14b14f6c25e3a99faeac3535ce5e9
Yaswanth C <yaswanthc4952@gmail.com>
2025-02-14 00:26:01 +0530

    model fields updated

--- linkadd/models.py
+++ linkadd/models.py
@@ -9,12 +9,12 @@ import datetime
 
 class Watchlist(models.Model):
     user=models.ForeignKey(User,on_delete=models.CASCADE)
-    product_url=models.URLField(max_length = 400,null=False)
+    product_url=models.TextField()
     domain = models.CharField(max_length = 30,null=False)
     product_name = models.CharField(max_length = 150,null=False)
     price = models.PositiveIntegerField(null=False)
     price_th = models.CharField(max_length = 10,null=False)
-    product_pic_url = models.URLField(max_length = 250,null=False)
+    product_pic_url = models.TextField()
     availability = models.CharField(max_length=20)
     date_added = models.DateTimeField(auto_now_add=True)
     pid = models.CharField(max_length=20,null=False)
